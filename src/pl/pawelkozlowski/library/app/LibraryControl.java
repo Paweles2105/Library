339f15bc6f107b052612637b2f22e4a53fd42a85
Paweł Kozłowski <paweles7@gmail.com>
2022-11-28 22:59:22 +0100

    enum

--- src/pl/pawelkozlowski/library/app/LibraryControl.java
+++ src/pl/pawelkozlowski/library/app/LibraryControl.java
@@ -6,21 +6,17 @@ import pl.pawelkozlowski.library.model.Library;
 import pl.pawelkozlowski.library.model.Magazine;
 
 public class LibraryControl {
-private final static int EXIT = 0;
-private final static int ADD_BOOKS = 1;
-private final static int ADD_MAGAZINES = 2;
-private final static int PRINT_BOOKS = 3;
-private final static int PRINT_MAGAZINES = 4;
+
 
     private DataReader dataReader = new DataReader();
     private Library library = new Library();
 
     public void controlLoop() {
-        int option;
+        Option option;
 
         do {
             printOptions();
-            option = dataReader.getInt();
+            option = Option.createFromInt(dataReader.getInt());
             switch (option){
                 case ADD_BOOKS:
                     addBook();
@@ -41,7 +37,7 @@ private final static int PRINT_MAGAZINES = 4;
                     System.out.println("Nie ma takiej opcji, wprowadź ponownie");
 
             }
-        } while (option != EXIT);
+        } while (option != Option.EXIT);
     }
 
     private void printMagazines() {
@@ -70,11 +66,10 @@ private final static int PRINT_MAGAZINES = 4;
 
     private void printOptions() {
         System.out.println("Wybierz opcję:");
-        System.out.println(EXIT + " - wyjście z programu");
-        System.out.println(ADD_BOOKS + " - dodanie nowej książki");
-        System.out.println(ADD_MAGAZINES + " - dodanie nowego magazynu");
-        System.out.println(PRINT_BOOKS + " - wyświetl dostępne książki");
-        System.out.println(PRINT_MAGAZINES + " - wyświetl dostępne magazyny");
+        for (Option value : Option.values()) {
+            System.out.println(value);
+
+        }
     }
 
 }
